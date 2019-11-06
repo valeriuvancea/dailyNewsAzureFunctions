286840aecc45bee99c2b3a6a038cfff8c76a79e3
Valeriu-Nicolas Vancea <valeriuvancea@gmail.com>
2019-11-06 17:06:50 +0100

    Add unlinkUserWithCategory function

---
+++ unlinkUserFromCategory/index.ts
@@ -0,0 +1,41 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import executeQuery from "../tools/sqlWorker";
+import * as sql from "mssql";
+import isNaturalNumber from "../tools/naturalNumberVerification"
+import errors from "../tools/errors";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const userId: string = req.params.userId;
+    const categoryId: string = req.params.categoryId;
+
+    if (isNaturalNumber(userId) && isNaturalNumber(categoryId)) {
+        const sqlQueryResponse: sql.IResult<any> | Error =
+            await executeQuery(`DELETE FROM usersWithCategories WHERE userId = ${userId} AND categoryId = ${categoryId}`);
+
+        if (sqlQueryResponse instanceof Error) {
+            context.res = {
+                body: { error: sqlQueryResponse.stack },
+                status: 500
+            };
+        }
+        else {
+            if (sqlQueryResponse.rowsAffected.length > 0) {
+                context.res.body = sqlQueryResponse;
+            }
+            else {
+                context.res = {
+                    body: { error: errors.userNotUpdated },
+                    status: 404
+                };
+            }
+        }
+    }
+    else {
+        context.res = {
+            body: { error: errors.userIdAndCategoryIdAreNotNaturalNumbers },
+            status: 412
+        };
+    }
+};
+
+export default httpTrigger;
